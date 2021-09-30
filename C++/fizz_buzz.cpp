fe44e8ba8adb92d854d128968e56810ba56b9f36
Ben C <bwc9876@gmail.com>
2021-09-30 14:54:29 -0700

    Added comments

--- C++/fizz_buzz.cpp
+++ C++/fizz_buzz.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main() {
 
+  // Begin our main loop of 1-50
   for (int i = 1; i <= 50; i++) {
 
-    string output = "";
+    string output = ""; 
 
-    if (i % 3 == 0) output += "Fizz";
-    if (i % 5 == 0) output += "Buzz";
+    if (i % 3 == 0) output += "Fizz"; // If our current number is a multiple of 3, add "Fizz to the output"
+    if (i % 5 == 0) output += "Buzz"; // If our current number is a multiple of 5, add "Fizz to the output"
 
-    if (output == "") output = to_string(i);
+    if (output == "") output = to_string(i); // If the output is empty (our number isn't a multiple of 3 or 5), we simply set it to our number
 
-    cout << output << endl;
+    cout << output << endl; // Show the user the output
 
   }
   
