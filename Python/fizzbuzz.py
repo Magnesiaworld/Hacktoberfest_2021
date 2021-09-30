1d52cc028e97251d854c995b812ff4a2c73e5ef0
Nemrud Demir <n@nemrud.de>
2021-10-01 00:29:55 +0200

    range to 50 instead of 100 and refactoring

--- Python/fizzbuzz.py
+++ Python/fizzbuzz.py
@@ -1,23 +1,10 @@
-# List of multiples.
-three = []
-five = []
-
-# List of numbers
-numbers = []
-
-# Get multiples and numbers.
-for n in range(100):
-    three.append((n + 1) * 3)
-    five.append((n + 1) * 5)
-    numbers.append(n + 1)
-
-# Magic(TM)
-for m in numbers:
-    if m in three and not m in five:
-        print("fizz")
-    elif m in five and not m in three:
-        print("buzz")
-    elif m in three and m in five:
+for n in range(50):
+    n += 1
+    if n % 3 == 0 and n % 5 == 0:
         print("fizzbuzz")
+    elif n % 3 == 0:
+        print("fizz")
+    elif n % 5 == 0:
+        print("buzz")
     else:
-        print(m)
+        print(n)
