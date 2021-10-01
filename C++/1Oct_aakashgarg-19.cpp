dfdde15939805f863463a9923c3613b18e7819f5
aakashgarg-19 <71538277+aakashgarg-19@users.noreply.github.com>
2021-10-02 04:10:02 +0530

    Update 1Oct_aakashgarg-19.cpp

--- C++/1Oct_aakashgarg-19.cpp
+++ C++/1Oct_aakashgarg-19.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int Isprime(int ele){
+    int x = 0;
+    for (int i = 1; i <= ele; i++){
+        if (ele % i == 0) x++;
+    }
+    if (x == 2) return 1;
+    return 0;
+}
+
+int main(){
+    int n;cin >> n;
+    int first = 1, second = 0, sum;
+    for (int i = 1; i <= n; i++){
+        sum = first + second;
+        first = second;
+        second = sum;
+        if (Isprime(sum)||(sum%5 == 0)) cout << "0 ";
+        else cout << sum << " ";
+    }
+    return 0;
+}
